23ab61484fb1d76fa5d4c640aadc56916969ecd5
Matt Tucker <arcanemahuloq@gmail.com>
2024-02-18 20:49:01 -0600

    Fixed Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,19 @@
-# Create sample data for your models
 10.times do
-  # Create a user with associated profile
+  # Create a user and profile
   user = FactoryBot.create(:user)
   profile = FactoryBot.create(:profile, user: user)
 
-  # Create an anime with associated genres and people through cast and crew
+  # Create an anime with genres
   anime = FactoryBot.create(:anime)
   genres = FactoryBot.create_list(:genre, 3)
-  people = FactoryBot.create_list(:person, 5)
+ 
   anime.genres << genres
-  anime.people << people
+
 
   # Create an anime list for the user
   anime_list = FactoryBot.create(:anime_list, user: user, anime: anime)
 
-  # Create a review associated with the user, anime, and comments
+  # Create a review with the user, anime, and comments
   review = FactoryBot.create(:review, user: user, anime: anime)
   FactoryBot.create_list(:comment, 3, review: review, user: user)
 
@@ -29,5 +28,9 @@
 
     # Create comments on the original review
     FactoryBot.create(:comment, review: review, user: other_user)
+  end
+  5.times do
+    FactoryBot.create(:cast_and_crew, person: FactoryBot.create(:person), anime: anime)
+
   end
 end
