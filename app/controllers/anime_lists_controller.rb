64d37faf1c0b9daac5a331561b099d46fa3d3efa
mahuloq <95161346+mahuloq@users.noreply.github.com>
2024-02-19 19:56:56 -0600

    stuff

--- app/controllers/anime_lists_controller.rb
+++ app/controllers/anime_lists_controller.rb
@@ -2,18 +2,36 @@ class AnimeListsController < ApplicationController
 
 
   def show
-
+    @user = User.find_by(username: params[:username])
+    @anime_list = AnimeList.where(user_id: @user.id).where(list_type: params[:status]) 
+    render json: @anime_list
   end
 
   def create
-
+    @anime_list = AnimeList.new(anime_list_params)
+    if @anime_list.save
+      render json: @anime_list, status: :created
+    else
+      render json: @anime_list.errors, status: :unprocessable_entity
+    end
   end
 
   def update
-
+    anime_list = AnimeList.find_by(user_id: params[:user_id], anime_id: params[:anime_id])
+    anime_list.update(category: params[:list_type]) if anime_list
   end
 
   def destroy
+    anime_list = AnimeList.find_by(user_id: params[:user_id], anime_id: params[:anime_id], category: params[:list_type])
+    anime_list.destroy if anime_list
+    head :no_content
+  end
+  
 
+
+  private
+
+  def anime_list_params
+    params.require(:anime_list).permit(:anime_id, :user_id, :list_type, :start_date, :end_date, :episodes_watched)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources  :animes
   resources  :users
   get 'profiles/:username', to: 'profiles#show', as: :profile
-  get 'animelists/:username', to: 'anime_lists#show', as: :anime_list
-
+  get 'animelist/:username', to: 'anime_lists#show', as: :anime_list
+  resources :anime_lists, only: [:create, :update, :destroy]
 end
   
