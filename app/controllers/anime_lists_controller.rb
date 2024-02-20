8e8091f7c13b497648d051a69043936420d0e269
mahuloq <95161346+mahuloq@users.noreply.github.com>
2024-02-19 18:55:50 -0600

    Started work on anime_list controller, changing routes

---
+++ app/controllers/anime_lists_controller.rb
@@ -0,0 +1,19 @@
+class AnimeListsController < ApplicationController
+
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   resources  :animes
   resources  :users
-  
+  get 'profiles/:username', to: 'profiles#show', as: :profile
+  get 'animelists/:username', to: 'anime_lists#show', as: :anime_list
+
 end
   
