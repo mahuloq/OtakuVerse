98da8ba81334013cb7355c89e9f15ff8f5f01ebe
mahuloq <95161346+mahuloq@users.noreply.github.com>
2024-03-11 17:55:12 -0500

    Modified CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins '*'
+      origins 'http://localhost:4200'
+      resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    end
+    allow do
+      origins 'https://fe-otaku-verse.vercel.app/'
       resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
     end
   end
