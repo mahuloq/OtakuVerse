e45cc6efce5a2341e9df8bbeca7e659c63d5725d
mahuloq <95161346+mahuloq@users.noreply.github.com>
2024-03-11 19:54:12 -0500

    spelling correction

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
 
     def create
         user = User.new(user_params)
-    
         if user.save
             render json: user, status: :created
         else
