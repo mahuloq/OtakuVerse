4760e368cac56c90ef802475af1c59301f1271ff
mahuloq <95161346+mahuloq@users.noreply.github.com>
2024-03-14 20:42:07 -0500

    Fixed sessions controller, seems like it got messed up on accident

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,16 +2,12 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(username: params[:username])
     if user&.authenticate(params[:password])
-      
       token = jwt_encode(user_id: user.id)
-      
       render json: { token: token }, status: :ok
     else
       render json: { error: 'Invalid username or password' }, status: :unauthorized
     end
-    
   end
-
 
   private
 
