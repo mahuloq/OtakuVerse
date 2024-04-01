49cab1ab787a8bac9925ec93fa97a8f4a98a1060
Matt Tucker <arcanemahuloq@gmail.com>
2024-03-31 21:57:21 -0500

    Added Rails.application routes host to Development and production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local = false
-Rails.application.routes.default_url_options[:host]= "https://otakuverse.onrender.com"
+  Rails.application.routes.default_url_options[:host] = 'https://otakuverse.onrender.com'
   # Ensures that a master key has been made available in ENV["RAILS_MASTER_KEY"], config/master.key, or an environment
   # key such as config/credentials/production.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
